1a9f8eb21dfd0402194da146ce42693da6c61e2b
Виктор <viktorgirda@yandex.ru>
2023-04-02 12:03:34 +0300

    Декомпозировал страницы авторизации

---
+++ src/components/header/header-sign-in/header-sign-in.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from "../../logo/logo";
+
+const HeaderSignIn = () => {
+  return (
+    <header className="page-header user-page__head">
+      <Logo />
+
+      <h1 className="page-title user-page__title">Sign in</h1>
+    </header>
+  );
+};
+
+export default HeaderSignIn;
